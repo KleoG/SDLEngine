130ee262585b38652db3147e214710f9af762bae
KleoG <kleogeorgiou@hotmail.com>
2016-05-29 20:46:46 +0100

    Rename main.h to animal/main.h

--- main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-extern const int SCREEN_WIDTH;
-extern const int SCREEN_HEIGHT;
-
-extern SDL_Window *window;
-extern SDL_Renderer *renderer;
-extern SDL_Event event;
